cb7e8d14db247061e77ac093f6e4efc58a728155
Gian Moran <gpmoran_@outlook.com>
2020-01-28 19:32:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
-  def initialize()
+  attr_reader :title 
+  attr_accessor :author, :page_count, :genre
+  def initialize(title)
+    @title = title 
+  end
   
-  
+  def turn_page
+  end
 end
 
